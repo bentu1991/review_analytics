0e558d51588b2f06bfa6967e9297f4f5ca890f04
Ben Tu <bentu1991@gmail.com>
2021-08-09 14:08:46 -0700

    good and bad review filter

--- read.py
+++ read.py
@@ -5,8 +5,6 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		count += 1
 		data.append(line)
-		if count % 1000 == 0:
-			print(count)
 print('finish loading, there are', len(data), ' reviews')
 
 
@@ -15,9 +13,12 @@ for d in data:
 	if len(d) < 100:
 		new.append(d)
 print('theres total of', len(new), 'reviews under 100 letters')
-	
 
-	
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('there are total of', len(good), 'good reviews')	
 
-
-	
+bad = [d for d in data if 'bad' in d]
+print('there are total of', len(bad), 'bad reviews')
