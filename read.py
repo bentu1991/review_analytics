aa39a987fef63f099d5fd452c1bed1d4dc4a0024
Ben Tu <bentu1991@gmail.com>
2021-08-09 11:45:29 -0700

    calc avg reviews len

--- read.py
+++ read.py
@@ -7,4 +7,13 @@ with open('reviews.txt', 'r') as f:
 		data.append(line)
 		if count % 1000 == 0:
 			print(count)
-print(len(data))
+print('finish loading, there are', len(data), ' reviews')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	
+
+print('avg review length are', sum_len/ len(data))
+
+	
